6aac8990a96b05a223dd5ca768adc03a4871e053
David Holmes <davidholmesnyc@gmail.com>
2015-04-19 12:46:33 -0400

    better error handling

--- typeof.js
+++ typeof.js
@@ -2,12 +2,17 @@
 /* better typeof than the default. For more information on why the default can suck sometimes visit this https://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/  */
 	this.typeof = function(string,callback) {
 		var type = ({}).toString.call(this).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+		this.error = function(callback){
+			return callback(type)
+		}
 		if(callback && string === type){
-	  	return callback( type )
+			callback( type )
+	  	return this
+		}else{
+			return this
 		}
 	  return type
 	}
-
 	Function.prototype.typeof = this.typeof
 	String.prototype.typeof = this.typeof
 	Object.prototype.typeof = this.typeof
